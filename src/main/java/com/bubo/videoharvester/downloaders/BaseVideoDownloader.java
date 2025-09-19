ac3725088492aa6cc39b25d20591067526c9fccb
LyubenIvanov <lubenivanov@gmail.com>
2025-09-19 23:39:39 +0300

    Refactored video titles logic

--- src/main/java/com/bubo/videoharvester/downloaders/BaseVideoDownloader.java
+++ src/main/java/com/bubo/videoharvester/downloaders/BaseVideoDownloader.java
@@ -11,11 +11,18 @@ import org.springframework.beans.factory.annotation.Value;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public abstract class BaseVideoDownloader {
 
     @Value("${videoharvester.video.download.script}")
     protected String downloadScript;
+
+    private static final Pattern SEASON_PATTERN = Pattern.compile("[Сс][Ее][Зз][Оо][Нн]\\s*[-:]*\\s*(\\d+)", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+    private static final Pattern EPISODE_PATTERN = Pattern.compile("[Ее][Пп][Ии][Зз][Оо][Дд]\\s*[-:]*\\s*(\\d+)", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+    private static final Pattern PART_PATTERN = Pattern.compile("[Чч][Аа][Сс][Тт]\\s*[-:]*\\s*(\\d+)", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
 
     protected abstract String getCssQuery();
 
@@ -30,7 +37,7 @@ public abstract class BaseVideoDownloader {
             Document doc = Jsoup.connect(show.getUrl()).get();
 
             return doc.select(getCssQuery()).stream().filter(element -> !element.children().isEmpty())
-                    .map(element -> parseVideo(element, show)).toList();
+                    .map(element -> parseVideo(element, show)).filter(Objects::nonNull).toList();
 
         } catch (IOException e) {
             getLogger().error("Error message: ", e);
@@ -67,9 +74,20 @@ public abstract class BaseVideoDownloader {
 
         Element linkElement = videoElement.selectFirst("a");
         String videoUrl = linkElement != null
-                          ? linkElement.absUrl("href")
-                          : "";
+                ? linkElement.absUrl("href")
+                : "";
         String title = extractTitle(videoElement);
-        return new Video(title, videoUrl, show);
+
+        Matcher seasonMatcher = SEASON_PATTERN.matcher(title);
+        Matcher episodeMatcher = EPISODE_PATTERN.matcher(title);
+        Matcher partMatcher = PART_PATTERN.matcher(title);
+
+        if (!seasonMatcher.find() || !episodeMatcher.find()) {
+            return null;
+        }
+        int season = Integer.parseInt(seasonMatcher.group(1));
+        int episode = Integer.parseInt(episodeMatcher.group(1));
+        String part = partMatcher.find() ? ".part" + partMatcher.group(1) : "";
+        return new Video(show.getTitle() + String.format(".S%02dE%02d", season, episode) + part, videoUrl, show);
     }
 }
